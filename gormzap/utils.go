80706e786c4c3c2e768d669dd3b14c5225fb9c14
thinkgos <thinkgo@aliyun.com>
2023-06-01 18:13:52 +0800

    fix: fix bug

--- gormzap/utils.go
+++ gormzap/utils.go
@@ -16,7 +16,7 @@ func FileWithLineNum(skipPackages ...string) string {
 	// the second caller usually from gorm internal, so set i start from 2
 	for i := 2; i < 15; i++ {
 		_, file, line, ok := runtime.Caller(i)
-		if ok && skipPackage(file, skipPackages...) {
+		if ok && !skipPackage(file, skipPackages...) {
 			return file + ":" + strconv.FormatInt(int64(line), 10)
 		}
 	}
@@ -25,17 +25,17 @@ func FileWithLineNum(skipPackages ...string) string {
 }
 
 func skipPackage(file string, skipPackages ...string) bool {
-	skip := func() bool {
-		if strings.Contains(file, gormPackage) ||
-			strings.Contains(file, gormzapPackage) {
-			return true
-		}
-		for _, p := range skipPackages {
-			if strings.Contains(file, p) {
-				return true
-			}
-		}
+	if strings.HasSuffix(file, "_test.go") {
 		return false
 	}
-	return !skip() || strings.HasSuffix(file, "_test.go")
+	if strings.Contains(file, gormPackage) ||
+		strings.Contains(file, gormzapPackage) {
+		return true
+	}
+	for _, p := range skipPackages {
+		if strings.Contains(file, p) {
+			return true
+		}
+	}
+	return false
 }
